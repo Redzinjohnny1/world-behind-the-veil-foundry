b3521c7a81b91e83b7d538ee01108f139fe8b87e
Redzinjohnny1 <redthekiller@gmail.com>
2026-05-25 17:07:04 -0300

    Normalize item type select in any dialog with type field

--- scripts/twbv-system.js
+++ scripts/twbv-system.js
@@ -41,7 +41,7 @@ function summarizeItemActiveEffects(item) {
 
 
 
-const TWBV_ITEM_CREATE_ORDER = ["arma", "armadura", "consumable", "equipamento", "vantagem", "desvantagem", "habilidadeEspecial"];
+const TWBV_ITEM_CREATE_ORDER = ["arma", "armadura", "consumable", "vantagem", "desvantagem", "habilidadeEspecial"];
 
 
 function twbvApplyItemTypeOrderConfig() {
@@ -1961,8 +1961,9 @@ Hooks.once("init", () => {
 });
 
 Hooks.on("renderDialog", (app, html) => {
-  const title = String(app?.title ?? "").toLowerCase();
-  if (title.includes("criar") && title.includes("item")) twbvNormalizeItemCreateTypeSelect(html);
+  const host = html?.[0] ?? html;
+  const hasItemTypeSelect = Boolean(host?.querySelector?.('select[name="type"]'));
+  if (hasItemTypeSelect) twbvNormalizeItemCreateTypeSelect(html);
 });
 
 Hooks.on("renderChatMessage", (message, html) => {
